d455f2bceb7634b05fba577848ac1ba27877f257
Sreeram Venkatarao <sreeram.v181@gmail.com>
2020-05-16 18:04:08 -0400

    Update textract.py
    Modified code

--- textract.py
+++ textract.py
@@ -2,28 +2,15 @@
 
 import pandas as pd
 import requests
+import pickle
 from bs4 import BeautifulSoup
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from nltk.tokenize import RegexpTokenizer
 from time import sleep
-
-# def tag_visible(element):
-#     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-#         return False
-#     if isinstance(element, Comment):
-#         return False
-#     return True
-#
-# def text_from_html(body):
-#     soup = BeautifulSoup(body, 'html.parser')
-#     texts = soup.findAll(text=True)
-#     visible_texts = filter(tag_visible, texts)
-#     return u" ".join(t.strip() for t in visible_texts)
-# #
-# html = urllib.request.urlopen('http://www.nytimes.com/2009/12/21/us/21storm.html').read()
-# print(text_from_html(html))
-# https://www.wellcare.com/Kentucky/Providers/Clinical-Guidelines/CCGs/CCG-List
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 def extract_text_from_urls(URLS):
     webpage_texts = []
@@ -33,9 +20,6 @@ def extract_text_from_urls(URLS):
             res = requests.get(url)
             html_page = res.content
             soup = BeautifulSoup(html_page, 'html.parser')
-            if soup.contains_replacement_characters == True:
-                break
-
             for paragraph in soup.find_all('p'):
                 currURL += str(paragraph.text) + " "
             webpage_texts.append(currURL)
@@ -51,7 +35,24 @@ def bagOfWords(extracted_text):
     return text_counts
 
 def train_classifier(X, y):
-    train_x, train_y, test_x, test_y = train_test_split(X, y, test_size=.2)
+    X_train, y_train, X_test, y_test = train_test_split(X, y, test_size=.2)
+    gnb = GaussianNB()
+    y_pred = gnb.fit(X_train, y_train).predict(X_test)
+    print("Number of mislabeled points out of a total %d points : %d" (X_test.shape[0], (y_test != y_pred).sum()))
+
+def bucketize_classification(classifications):
+    # Correct = 0
+    # Error: Related product = 1
+    # Error: Unrelated product = 2
+    bucketized_classifications = []
+    for category in classifications:
+        if category == "Correct":
+            bucketized_classifications.append(0)
+        elif category == "Error: Related product":
+            bucketized_classifications.append(1)
+        else:
+            bucketized_classifications.append(2)
+    return bucketized_classifications
 
 
 def main():
@@ -62,13 +63,22 @@ def main():
     URLS = URLS_df['Webpage'].to_list()
     classifications = classifications_df['Class'].to_list()
 
+    # Convert classifications to integer buckets
+    bucketized_classifications = bucketize_classification(classifications[:109])
+
     #Extract text from websites
     extracted_text = extract_text_from_urls(URLS[:109])
-    print(extracted_text)
+    # print(extracted_text)
 
     #Create bag of words features
     text_counts = bagOfWords(extracted_text)
-    
+    with open('text_counts.pickle', 'wb') as f:
+        pickle.dump(text_counts, f)
+    with open('classes.pickle', 'wb') as g:
+        pickle.dump(classifications, g)
+
+    train_classifier(text_counts, classifications)
+
 
 
 if __name__ == "__main__":
