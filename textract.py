e3793d212d8b15e688c32da0350910d76f204f2c
smundada <smundada@users.noreply.github.com>
2020-06-08 17:26:22 -0700

    code for precision, recall classification added

--- textract.py
+++ textract.py
@@ -6,12 +6,14 @@ from sklearn.model_selection import train_test_split
 from time import sleep
 from sklearn.naive_bayes import GaussianNB
 from utils import extract_text_from_urls, get_urls_classes_from_file, bagOfWords, bucketize_classification
+from sklearn.metrics import confusion_matrix, classification_report
 
 def train_classifier(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
     gnb = GaussianNB()
     y_pred = gnb.fit(X_train, y_train).predict(X_test)
     print("Number of mislabeled points out of a total {} points : {}".format(X_test.shape[0], (y_test != y_pred).sum()))
+    print(classification_report(y_test, y_pred))
 
 def main():
     URLS, classifications = get_urls_classes_from_file("Rawcrawl_data.csv")
